54b51605ea07799108484191ac98b5221ca2857e
岩本友太 <iwamotoyuuta@iwamotoyuutanoMacBook-Air.local>
2019-10-29 11:52:37 +0900

    FCヲツクッタ

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
 
-
-function App() {
+// function App() {
   
-  return(
-  <div>
-  <label htmlFor="bar">
-    bar
-    </label><input type="text" onChange={() => {console.log("hihi")}} />
-  </div>
+//   return(
+//   <div>
+//   <label htmlFor="bar">
+//     bar
+//     </label><input type="text" onChange={() => {console.log("hihi")}} />
+//   </div>
 
-  );
+//   );
+// }
+
+const App = () => {
+  return(
+<div>
+
+<Cat />
+<Cat />
+<Cat />
+
+</div>
+
+  ) 
+}
+const Cat = () => {
+  return <div>nyan!</div>
 }
 
 export default App;

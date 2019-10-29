94d37ca0fa2789b60ec0ea3d435c061284fc0803
岩本友太 <iwamotoyuuta@iwamotoyuutanoMacBook-Air.local>
2019-10-29 13:23:52 +0900

    props study

--- src/App.js
+++ src/App.js
@@ -13,19 +13,31 @@ import React from 'react';
 // }
 
 const App = () => {
+  const profiles = [
+    {name: "taro" , age: 10},
+    {name: "hanako" , age: 5},
+    {name: "nobady"}
+  ]
   return(
 <div>
+  {
+    profiles.map((profile, index) => {
+      return( <User name={profile.name} age={profile.age} key={index}/> );
+    })
 
-<Cat />
-<Cat />
-<Cat />
+  }
 
 </div>
 
   ) 
 }
-const Cat = () => {
-  return <div>nyan!</div>
+const User = (props) => {
+  return <div>{props.name} and {props.age} years old </div>
+
+}
+
+User.defaultProps = {
+age:1
 }
 
 export default App;
